78cc39c5c4a602ad5648d23d3f7a4dc0d424b3ab
Eugene Denisenko <caravus@craft.sc>
2024-10-10 03:08:26 +0500

    work

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,6 @@
 set -ex
 export CUDACXX=/usr/local/cuda-12/bin/nvcc
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DSD_CUBLAS=ON -DCMAKE_CUDA_ARCHITECTURES=OFF
+cmake .. -DCMAKE_BUILD_TYPE=Release -DSD_CUBLAS=ON -DSD_BUILD_EXAMPLES=OFF -DCMAKE_CUDA_ARCHITECTURES=OFF -DSD_BUILD_SHARED_LIBS=ON
 cmake --build . --config Release -j 10
-mv -f ./bin/libsd-abi.so /code/github/ring-c/go-web-diff/pkg/bind/deps/linux/
+mv -f ./bin/libstable-diffusion.so /code/github/ring-c/go-web-diff/pkg/bind/deps/linux/libsd-abi.so
